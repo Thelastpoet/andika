85a2fb31e724ff218395754bb2fb227e5a0a6865
thelastpoet <emcie4@gmail.com>
2023-05-07 07:00:16 +0300

    Implemented Auto Paragraphs

--- src/utils/andika-ai.js
+++ src/utils/andika-ai.js
@@ -1,32 +1,61 @@
-import { __ } from '@wordpress/i18n';
+import { createBlock } from '@wordpress/blocks';
 
-import Andika_OpenAI_API from './andika-api';
+import AndikaOpenAI from './andika-api';
 
-export async function generateText(prompt, content, setContent) {
-  const andikaAPI = new Andika_OpenAI_API(andika);
+export async function generateText(prompt, content, setContent, insertBlocks, clientId) {
+  const andikaAPI = new AndikaOpenAI(andika);
 
-  // Callback function for the streaming events
-  const andikaStreamEvents = (event) => {
+  let accumulatedText = '';
+
+  const andikaStreamEvents = async (event) => {
     if (event.type === 'event') {
-      if (event.data !== "[DONE]") {        
-      try {
-        const parsedData = JSON.parse(event.data);
-        const choices = parsedData.choices || [];
-        const json = choices[0]?.text ?? choices[0]?.message?.content ?? choices[0]?.delta?.content ?? '';
-        setContent((prevContent) => prevContent + json);
-      } catch (e) {
-        console.error('Error parsing JSON', e);
+      if (event.data !== "[DONE]") {
+        try {
+          const parsedData = JSON.parse(event.data);
+
+          const text =
+            parsedData.choices[0]?.text ??
+            parsedData.choices[0]?.message?.content ??
+            parsedData.choices[0]?.delta?.content ??
+            ''
+          ;
+
+          const cleanedText = text.replace(/^\n{1,2}/, '');
+
+          if (cleanedText) {
+            accumulatedText += cleanedText;
+
+            if (accumulatedText.includes('\n')) {
+
+              const paragraphs = accumulatedText.split(/\n+/);
+
+              // Filter out empty paragraphs
+              const nEP = paragraphs.filter((paragraph) => paragraph.trim() !== '');
+
+              const blocksToInsert = nEP.map((paragraph) =>
+                createBlock('andika-block/andika', { content: paragraph })
+              );
+
+              const currentIndex = wp.data.select('core/block-editor').getBlockIndex(clientId);
+
+              insertBlocks(blocksToInsert, currentIndex);
+
+              accumulatedText = '';
+
+            } else {
+              setContent(accumulatedText);
+            }
+          }
+        } catch (e) {
+          console.error('Error parsing JSON', e);
+        }
       }
-    }
     }
   };
 
   try {
-    await andikaAPI.andika_text(prompt, andikaStreamEvents);
+    await andikaAPI.andikaText(prompt, andikaStreamEvents);
   } catch (error) {
     console.error('Error in generateText', error);
   }
 }
-
-
-
